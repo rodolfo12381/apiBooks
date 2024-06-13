73850ea27f904a1125a039b1062e5bd98c7033de
Rodolfo Rocha Rodrigues <rodolfo.rocha@HINOVA.net>
2024-06-13 17:00:22 -0300

    adicionando estrutura de pastas, controllers e routes

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
