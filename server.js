abc80d536e43fd6838cfd2448ae3159d65e07be4
rodolfo12381 <rodolforrodrigues14@gmail.com>
2024-06-17 17:52:04 -0300

    adicionando tratamento de errors e middleware

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 dotenv.config({path: './config/config.env'});
 
+connectDB();
+
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+app.use(express.json());
 
 // Dev loggin middleware
 if (process.env.NODE_ENV === 'development') {
@@ -15,6 +21,13 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/bootcamps',bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+})
